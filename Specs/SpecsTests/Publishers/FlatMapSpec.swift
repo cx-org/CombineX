b9cc6e3bafeb1b2831f17ae5758eaf655cbf347d
Quentin Jin <jianstm@gmail.com>
2019-07-19 23:46:40 +0800

    Fix flatmap tests

--- Specs/SpecsTests/Publishers/FlatMapSpec.swift
+++ Specs/SpecsTests/Publishers/FlatMapSpec.swift
@@ -54,19 +54,16 @@ class FlatMapSpec: QuickSpec {
                         Publishers.Sequence<[Int], Never>(sequence: [$0, $0, $0])
                     }
                 
-                var received = 0
-                
                 let sub = CustomSubscriber<Int, Never>(receiveSubscription: { s in
-                    s.request(.max(1))
+                    s.request(.max(10))
                 }, receiveValue: { v in
-                    received += 1
-                    return received == 10 ? .none : .max(1)
+                    [1, 5].contains(v) ? .max(1) : .none
                 }, receiveCompletion: { c in
                 })
                 
                 pub.subscribe(sub)
                 
-                expect(sub.events.count).to(equal(received))
+                expect(sub.events.count).to(equal(19))
             }
             
             // MARK: 1.3 should complete when a sub-publisher sends an error
@@ -149,113 +146,17 @@ class FlatMapSpec: QuickSpec {
             }
         }
         
-        // MARK: - Release Resources
-        describe("Release Resources") {
-            
-            // MARK: 2.1 subscription should retain upstream, downstream and transform closure then only release upstream after upstream send finish
-            it("subscription should retain upstream, downstream and transform closure then only release upstream after upstream send finish") {
-                
-                weak var upstreamObj: PassthroughSubject<Int, Never>?
-                weak var downstreamObj: AnyObject?
-                weak var closureObj: CustomObject?
-                
-                var subscription: Subscription?
-                
-                do {
-                    let subject = PassthroughSubject<Int, Never>()
-                    upstreamObj = subject
-                    
-                    let obj = CustomObject()
-                    closureObj = obj
-                    
-                    let pub = subject.flatMap { _ -> Just<Int> in
-                        obj.run()
-                        return Just(1)
-                    }
-                    
-                    let sub = CustomSubscriber<Int, Never>(receiveSubscription: { (s) in
-                        subscription = s
-                        s.request(.unlimited)
-                    }, receiveValue: { v in
-                        return .none
-                    }, receiveCompletion: { s in
-                    })
-                    downstreamObj = sub
-                    
-                    pub.subscribe(sub)
-                }
-                
-                expect(upstreamObj).toNot(beNil())
-                expect(downstreamObj).toNot(beNil())
-                expect(closureObj).toNot(beNil())
-                
-                upstreamObj?.send(completion: .finished)
-                
-                expect(upstreamObj).to(beNil())
-                expect(downstreamObj).toNot(beNil())
-                expect(closureObj).toNot(beNil())
-                
-                _ = subscription
-            }
-            
-            // MARK: 2.2 subscription should retain upstream, downstream and transform closure then only release upstream after cancel
-            it("subscription should retain upstream, downstream and transform closure then only release upstream after cancel") {
-                
-                weak var upstreamObj: AnyObject?
-                weak var downstreamObj: AnyObject?
-                
-                weak var closureObj: CustomObject?
-                
-                var subscription: Subscription?
-                
-                do {
-                    let subject = PassthroughSubject<Int, Never>()
-                    upstreamObj = subject
-                    
-                    let obj = CustomObject()
-                    closureObj = obj
-                    
-                    let pub = subject.flatMap { _ -> Just<Int> in
-                        obj.run()
-                        return Just(1)
-                    }
-                    
-                    let sub = CustomSubscriber<Int, Never>(receiveSubscription: { (s) in
-                        subscription = s
-                        s.request(.unlimited)
-                    }, receiveValue: { v in
-                        return .none
-                    }, receiveCompletion: { s in
-                    })
-                    downstreamObj = sub
-                    
-                    pub.subscribe(sub)
-                }
-                
-                expect(upstreamObj).toNot(beNil())
-                expect(downstreamObj).toNot(beNil())
-                expect(closureObj).toNot(beNil())
-                
-                subscription?.cancel()
-                
-                expect(upstreamObj).to(beNil())
-                expect(downstreamObj).toNot(beNil())
-                expect(closureObj).toNot(beNil())
-            }
-        }
-        
         // MARK: - Concurrent
         describe("Concurrent") {
             
-            // MARK: 3.1 should send as many values ad demand event if there are sent concurrently
-            fit("should send as many values ad demand event if there are sent concurrently") {
-                let sequence = Publishers.Sequence<[Int], Never>(sequence: [0, 1, 2])
+            // MARK: 2.1 should send as many values ad demand event if there are sent concurrently
+            it("should send as many values ad demand event if there are sent concurrently") {
+                let sequence = Publishers.Sequence<[Int], Never>(sequence: Array(0..<100))
                 
-                let subjects = [
-                    PassthroughSubject<Int, Never>(),
-                    PassthroughSubject<Int, Never>(),
-                    PassthroughSubject<Int, Never>(),
-                ]
+                var subjects: [PassthroughSubject<Int, Never>] = []
+                for _ in 0..<100 {
+                    subjects.append(PassthroughSubject<Int, Never>())
+                }
                 
                 let pub = sequence.flatMap { (i) -> PassthroughSubject<Int, Never> in
                     return subjects[i]
@@ -274,7 +175,7 @@ class FlatMapSpec: QuickSpec {
                 
                 100.times { i in
                     DispatchQueue.global().async(group: g) {
-                        subjects.randomElement()!.send(i)
+                        subjects[i].send(i)
                     }
                 }
                 
