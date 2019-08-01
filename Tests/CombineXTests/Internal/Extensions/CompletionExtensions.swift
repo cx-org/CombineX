09168b7824495009d251e8921d830c8e53525064
Quentin Jin <jianstm@gmail.com>
2019-08-01 21:56:24 +0800

    split types from combinex into separate folder

--- Tests/CombineXTests/Internal/Extensions/CompletionExtensions.swift
+++ Tests/CombineXTests/Internal/Extensions/CompletionExtensions.swift
@@ -18,24 +18,3 @@ extension Subscribers.Completion {
     }
 }
 
-// MARK: - test
-extension Subscribers.Completion {
-    
-    var isFinished: Bool {
-        switch self {
-        case .finished:
-            return true
-        case .failure:
-            return false
-        }
-    }
-    
-    var isFailure: Bool {
-        switch self {
-        case .failure:
-            return true
-        case .finished:
-            return false
-        }
-    }
-}
