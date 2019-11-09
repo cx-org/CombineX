56141bdaf8d4a452384d393670aaa4f7b3c193cf
Xander Deng <dengxiang2010@gmail.com>
2019-11-09 16:16:22 +0800

    remove boilerplate CustomStringConvertible documentation

--- Sources/CombineX/AnySubscriber.swift
+++ Sources/CombineX/AnySubscriber.swift
@@ -13,29 +13,6 @@ public struct AnySubscriber<Input, Failure> : Subscriber, CustomStringConvertibl
     @usableFromInline
     let box: SubscriberBox<Input, Failure>
     
-    /// A textual representation of this instance.
-    ///
-    /// Calling this property directly is discouraged. Instead, convert an
-    /// instance of any type to a string by using the `String(describing:)`
-    /// initializer. This initializer works with any type, and uses the custom
-    /// `description` property for types that conform to
-    /// `CustomStringConvertible`:
-    ///
-    ///     struct Point: CustomStringConvertible {
-    ///         let x: Int, y: Int
-    ///
-    ///         var description: String {
-    ///             return "(\(x), \(y))"
-    ///         }
-    ///     }
-    ///
-    ///     let p = Point(x: 21, y: 30)
-    ///     let s = String(describing: p)
-    ///     print(s)
-    ///     // Prints "(21, 30)"
-    ///
-    /// The conversion of `p` to a string in the assignment to `s` uses the
-    /// `Point` type's `description` property.
     public var description: String {
         return "AnySubscriber"
     }
--- Sources/CombineX/CombineIdentifier.swift
+++ Sources/CombineX/CombineIdentifier.swift
@@ -16,29 +16,6 @@ public struct CombineIdentifier : Hashable, CustomStringConvertible {
         self.id = UInt(bitPattern: ObjectIdentifier(obj))
     }
     
-    /// A textual representation of this instance.
-    ///
-    /// Calling this property directly is discouraged. Instead, convert an
-    /// instance of any type to a string by using the `String(describing:)`
-    /// initializer. This initializer works with any type, and uses the custom
-    /// `description` property for types that conform to
-    /// `CustomStringConvertible`:
-    ///
-    ///     struct Point: CustomStringConvertible {
-    ///         let x: Int, y: Int
-    ///
-    ///         var description: String {
-    ///             return "(\(x), \(y))"
-    ///         }
-    ///     }
-    ///
-    ///     let p = Point(x: 21, y: 30)
-    ///     let s = String(describing: p)
-    ///     print(s)
-    ///     // Prints "(21, 30)"
-    ///
-    /// The conversion of `p` to a string in the assignment to `s` uses the
-    /// `Point` type's `description` property.
     public var description: String {
         return "0x" + String(self.id, radix: 16)
     }
