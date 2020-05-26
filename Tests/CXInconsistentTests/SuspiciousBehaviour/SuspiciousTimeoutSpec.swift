3770f382d024fd1601d10fdd911fa64bf5938b76
Xander Deng <dengxiang2010@gmail.com>
2020-05-26 14:37:56 +0800

    add SuspiciousTimeoutSpec

---
+++ Tests/CXInconsistentTests/SuspiciousBehaviour/SuspiciousTimeoutSpec.swift
@@ -0,0 +1,28 @@
+import CXShim
+import CXTestUtility
+import Dispatch
+import Nimble
+import Quick
+
+class SuspiciousTimeoutSpec: QuickSpec {
+    
+    override func spec() {
+        
+        afterEach {
+            TestResources.release()
+        }
+        
+        it("should not send value without demand") {
+            let subject = PassthroughSubject<Int, Never>()
+            let pub = subject.timeout(.milliseconds(1), scheduler: DispatchQueue.global().cx)
+            let sub = makeTestSubscriber(Int.self, Never.self)
+            pub.subscribe(sub)
+            subject.send(0)
+            expect(sub.events[0].isSubscription) == true
+            // Combine.Publishers.Timeout leak value
+            expect(sub.events.count).toBranch(
+                combine: equal(2),
+                cx: equal(1))
+        }
+    }
+}
