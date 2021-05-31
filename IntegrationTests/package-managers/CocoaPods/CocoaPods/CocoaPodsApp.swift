9d3600fccf4c0d16991d6c98545b1f10e4ca0253
luoxiu <luoxiustm@gmail.com>
2021-05-31 21:09:25 +0800

    add integration tests for cocoapods

---
+++ IntegrationTests/package-managers/CocoaPods/CocoaPods/CocoaPodsApp.swift
@@ -0,0 +1,18 @@
+//
+// CocoaPodsApp.swift
+//
+//
+// Created by Quentin Jin on 2021/5/31.
+//
+
+
+import SwiftUI
+
+@main
+struct CocoaPodsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ IntegrationTests/package-managers/CocoaPods/Pods/Target Support Files/CombineX/CombineX-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_CombineX : NSObject
+@end
+@implementation PodsDummy_CombineX
+@end
