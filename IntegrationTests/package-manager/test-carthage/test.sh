9441d708bbe99a0b93b3aa31ff5214a75917c563
luoxiu <luoxiustm@gmail.com>
2021-06-01 08:40:27 +0800

    install pm before test

--- IntegrationTests/package-manager/test-carthage/test.sh
+++ IntegrationTests/package-manager/test-carthage/test.sh
@@ -1,5 +1,6 @@
 #! /usr/bin/env bash
 
+brew install carthage
 
 GIT_PATH=$(cd ../../../ && pwd)
 echo "git \"${GIT_PATH}\"" >| 'Cartfile'
--- IntegrationTests/package-manager/test-cocoapods/test.sh
+++ IntegrationTests/package-manager/test-cocoapods/test.sh
@@ -1,5 +1,7 @@
 #! /usr/bin/env bash
 
+brew install cocoapods
+
 pod install
 
 xcodebuild -scheme test-cocoapods -workspace test-cocoapods.xcworkspace -sdk iphonesimulator clean build
